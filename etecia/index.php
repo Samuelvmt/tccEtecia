ae17835d9b4b41f0d794bf97b3db588b9c10ae94
samuelvmt <samu.7v@gmail.com>
2024-10-01 17:42:40 -0300

    tentando fazer conexão com banco de dados

--- etecia/index.php
+++ etecia/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-
+<?php 
+	include_once 'php/conexao.php';
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
