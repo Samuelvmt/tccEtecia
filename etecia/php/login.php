43f880e3b76d1a5b4501887198b64b1d1d0cbf36
samuelvmt <samu.7v@gmail.com>
2024-10-21 22:05:29 -0300

    configurando login no php

--- etecia/php/login.php
+++ etecia/php/login.php
@@ -1,43 +1,10 @@
-
-<?php
-/*
-$login = $_POST["login"];
-$entrar = $_POST["entrar"];
-$senha = md5($_POST["senha"]);
-$connect = new mysqli("localhost", "etecia1", "1234567");
-$db = mysql_select_db("dbEscola");
-if ($login == "" || $login == null) {
-  echo "<script language='javascript' type='text/javascript'>
-  alert('O campo login deve ser preenchido');window.location.href='
-  login.html';</script>";
-} else if ($senha == "" || $senha == null) {
-  echo "<script language='javascript' type='text/javascript'>
-  alert('O campo senha deve ser preenchido');window.location.href='
-  login.html';</script>";
-} else if (isset($entrar)) {
-
-  $verifica = mysql_query("SELECT * FROM tbUsuario WHERE login =
-    '$login' AND senha = '$senha'") or die("erro ao selecionar");
-  if (mysql_num_rows($verifica) <= 0) {
-    echo "<script language='javascript' type='text/javascript'>
-        alert('Login e/ou senha incorretos');window.location
-        .href='login.html';</script>";
-    die();
-  } else {
-    setcookie("login", $login);
-    header("Location:index.php");
-  }
-}
-/* */
-?>
-
 <?php
 $login = $_POST["login"];
 $entrar = $_POST["entrar"];
-$senha = md5($_POST["senha"]);
+$senha = $_POST['senha'];
 
 // Estabelecendo a conexão com o banco de dados
-$connect = new mysqli("localhost", "etecia1", "1234567", "dbEscola");
+$connect = new mysqli("localhost", "etecia", "123456", "dbEscola");
 
 // Verificando se a conexão foi bem-sucedida
 if ($connect->connect_error) {
