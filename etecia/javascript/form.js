f5979e3a1028725bba7628e6084a73775e91d03b
samuelvmt <samu.7v@gmail.com>
2024-11-06 21:40:55 -0300

    personalizando páginas de aluno

--- etecia/javascript/form.js
+++
@@ -1,9 +0,0 @@
-// Obter o modal
-var modal = document.getElementById('id01');
-
-// Quando o usuário clicar em qualquer lugar fora do modal, o modal será fechado
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
